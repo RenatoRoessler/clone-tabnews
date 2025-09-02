42265711d15b72940587471bc8633edeff93646c
renatoroessler <renatoroessler@gmail.com>
2025-09-02 20:01:10 -0300

    adds wait-for-postgres.js scripts

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,21 @@
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+  exec(
+    "wsl docker exec postgres-dev pg_isready --host localhost",
+    handleReturn,
+  );
+
+  function handleReturn(error, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
+    }
+
+    console.log("\n-- Postgres está pronto e aceitando conexões \n");
+  }
+}
+process.stdout.write("\n\n -- aguardando Postgres aceitar conexões...");
+
+checkPostgres();
