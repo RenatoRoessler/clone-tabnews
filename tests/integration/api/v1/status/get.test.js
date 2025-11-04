15715a8e78abf912d5c94cf7faa0ffe8cbbc6793
renatoroessler <renatoroessler@gmail.com>
2025-11-04 18:51:17 -0300

    ed

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -13,7 +13,7 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
   expect(responseBody.update_at).toEqual(parsedUpdateAt);
 
-  expect(responseBody.dependecies.database.version).toEqual("16.6");
+  expect(responseBody.dependecies.database.version).toEqual("20.6");
   expect(responseBody.dependecies.database.max_connections).toEqual(100);
   expect(responseBody.dependecies.database.opened_connections).toEqual(1);
 });
